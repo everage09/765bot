f190511e765db9a25f906ac352ae0f8d2ec669f2
Everage <44075728+everage09@users.noreply.github.com>
2022-01-10 01:45:38 +0900

    Update gs.py

--- gs.py
+++ gs.py
@@ -3,7 +3,7 @@ from google.oauth2.service_account import Credentials  # 보안인증 모듈
 
 
 def GS():  # 스프레드 시트 불러오기
-    gc = gspread.service_account(filename="/Users/X5967T/AppData/Roaming/gspread/lucky-monument-320311-c8a8babc62f2.json")
+    gc = gspread.service_account(filename="Your file directory")
     sh = gc.open("전칠협이벤트기록")
     scopes = [
         'https://www.googleapis.com/auth/spreadsheets',
